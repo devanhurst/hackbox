2a8d8d778861e6ab7858f836292402c709e4938e
Devan Hurst <devan.hurst@gmail.com>
2024-04-10 14:36:01 -0400

    Use DOMPurify.

--- player-client/src/lib/markdown.ts
+++ player-client/src/lib/markdown.ts
@@ -1,6 +1,8 @@
-import xss from "xss";
+import DOMPurify from "dompurify";
 import { marked } from "marked";
 
 export default (text: string) => {
-  return xss(marked.parse(text) as string);
+  const html = marked.parse(text) as string;
+  const sanitized = DOMPurify.sanitize(html);
+  return sanitized;
 };
