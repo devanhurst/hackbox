433d2b879eccffa7787676121e258d66581501c0
Devan Hurst <devan.hurst@gmail.com>
2024-08-29 15:45:04 +0100

    Add sentry tracing to player client

--- player-client/src/main.ts
+++ player-client/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import components from "@/components";
+import * as Sentry from "@sentry/vue";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -11,6 +12,19 @@ import { faTwitch } from "@fortawesome/free-brands-svg-icons";
 library.add(faPaperPlane, faCheck, faTwitch);
 
 const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: "https://5bc56324afa78ccaafb83f1ad75bb800@o4507848851652608.ingest.us.sentry.io/4507848855388160",
+  integrations: [
+    Sentry.browserTracingIntegration({ router }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/app\.hackbox\.ca/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 app.use(router);
 
