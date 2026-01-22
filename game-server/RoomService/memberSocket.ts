e7e2a84ca17e83e56f6c5beb723e05830152e228
Devan Hurst <devan.hurst@gmail.com>
2026-01-22 13:00:25 +0000

    Add sentry logging for messages

--- game-server/RoomService/memberSocket.ts
+++ game-server/RoomService/memberSocket.ts
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/node";
 import type { Socket } from "socket.io";
 import { authenticateWithTwitch } from "../lib/twitch";
 import { Member } from "../models";
@@ -25,57 +26,61 @@ interface MemberPayload {
 }
 
 export default async ({ socket, roomService }: RegisterMemberInput) => {
-	const handshake = socket.handshake.query as unknown as Handshake;
-const handshakeMetadata =
-  (JSON.parse(handshake.metadata) as HandshakeMetadata) || {};
-const metadata = {
-  twitch: await authenticateWithTwitch(handshakeMetadata.twitchAccessToken),
-};
+  const handshake = socket.handshake.query as unknown as Handshake;
+  const handshakeMetadata =
+    (JSON.parse(handshake.metadata) as HandshakeMetadata) || {};
+  const metadata = {
+    twitch: await authenticateWithTwitch(handshakeMetadata.twitchAccessToken),
+  };
 
-socket.data = {
-  type: "member",
-  userId: handshake.userId,
-  userName: handshake.userName,
-  metadata,
-  state: {},
-};
+  socket.data = {
+    type: "member",
+    userId: handshake.userId,
+    userName: handshake.userName,
+    metadata,
+    state: {},
+  };
 
-socket.on("msg", async (payload: MemberPayload) => {
-  roomService.sendToHost({
-    event: "msg",
-    payload: {
+  socket.on("msg", async (payload: MemberPayload) => {
+    const hostPayload = {
       from: socket.data.userId,
       event: payload.event,
       message: payload,
       timestamp: Date.now(),
-    },
+    };
+
+    roomService.sendToHost({
+      event: "msg",
+      payload: hostPayload,
+    });
+
+    Sentry.logger.info(`[${roomService.room.code}] Message sent to host`, {
+      roomCode: roomService.room.code,
+      event: "msg",
+      payload: hostPayload,
+    });
   });
 
-  console.log(
-    `[${roomService.room.code}] ${socket.data.userName}: ${payload.value} (${payload.event})`,
-  );
-});
-
-socket.on("change", async (payload: MemberPayload) => {
-  roomService.sendToHost({
-    event: "change",
-    payload: {
-      from: socket.data.userId,
-      event: payload.event,
-      message: payload,
-      timestamp: Date.now(),
-    },
-  });
-});
-
-socket.on("disconnect", async () => {
-  const member = await Member.find({
-    userId: socket.data.userId,
-    roomCode: roomService.room.code,
+  socket.on("change", async (payload: MemberPayload) => {
+    roomService.sendToHost({
+      event: "change",
+      payload: {
+        from: socket.data.userId,
+        event: payload.event,
+        message: payload,
+        timestamp: Date.now(),
+      },
+    });
   });
 
-  if (member) await member.save({ online: false });
+  socket.on("disconnect", async () => {
+    const member = await Member.find({
+      userId: socket.data.userId,
+      roomCode: roomService.room.code,
+    });
 
-  roomService.updateHostState();
-});
+    if (member) await member.save({ online: false });
+
+    roomService.updateHostState();
+  });
 };
